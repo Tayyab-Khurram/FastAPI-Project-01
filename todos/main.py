da0421a9bca1c54e0897d059d434f86f79ec2ab4
Tayyab Khurram <tayyabsheikh.pakistan@gmail.com>
2025-12-29 13:15:57 -0600

    Implement student management endpoints
    Added student management features including adding and deleting students, and searching by name or RollNo.

--- todos/main.py
+++ todos/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI()
@@ -12,6 +13,11 @@ students = [
 ]
 
 
+class Student(BaseModel):
+    name: str
+    RollNo: int
+
+
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
@@ -23,8 +29,29 @@ def read_todo(id: int, userName: str, RollNo: int):
 
 
 @app.get("/students")
-def read_students():
+def read_students(name: str = None, RollNo: int = None):
+    if name is None and RollNo is None:
+        return students
+    for std in students:
+        if name == std.get('name') and RollNo == std.get('RollNo'):
+            return {"name": name, "RollNo": RollNo}
+    return {"message": "Student not found"}
+
+
+@app.post("/add_student")
+def add_student(student: Student):
+    new_student = {"name": student.name, "RollNo": student.RollNo}
+    students.append(new_student)
     return students
+
+
+@app.delete('/delete_student')
+def delete_student(name: str, RollNo: int):
+    for std in students:
+        if name == std.get('name') and RollNo == std.get('RollNo'):
+            students.remove(std)
+            return students
+    return {"message": "Student not found"}
 
 
 @app.get("/todos")
