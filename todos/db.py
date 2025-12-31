93363108642db3120671defced819a03600e31a6
Tayyab Khurram <tayyabsheikh.pakistan@gmail.com>
2025-12-31 12:19:29 -0600

    Implement add_students and get_students functions
    Added functions to add and retrieve students from the database.

--- todos/db.py
+++ todos/db.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel, Field, create_engine
+from sqlmodel import SQLModel, Field, create_engine, Session, select
 
 connection_string = "sqlite:///stdbase.db"
 connection = create_engine(connection_string)
@@ -8,8 +8,24 @@ class Students(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     name: str
     age: int
-    address: str
-    phone: int
+    phone: str
+    address: str | None = None
 
 
 SQLModel.metadata.create_all(connection)
+
+
+def add_students(student_list):
+    with Session(connection) as session:
+        for student in student_list:
+            session.add(student)
+        session.commit()
+    return {"message": "Students added successfully"}
+
+
+def get_students():
+    with Session(connection) as session:
+        statement = select(Students)
+        results = session.exec(statement)
+        students_data = results.all()
+    return students_data
