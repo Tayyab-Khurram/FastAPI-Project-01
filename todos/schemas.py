27613b8ab436ed233cf1b1dcb7a74e601d1fc6b8
Tayyab Khurram <tayyabsheikh.pakistan@gmail.com>
2026-01-01 13:20:24 -0600

    Refactor Todos class and add UpdateTodo schema

--- todos/schemas.py
+++ todos/schemas.py
@@ -1,8 +1,17 @@
 from sqlmodel import SQLModel, Field
 
-class Todos(SQLModel, table=True):
+
+class Todo(SQLModel, table=True):
+    __tablename__ = 'todo'
     id: int = Field(default=None, primary_key=True)
     title: str
     description: str
     priority: int | None = None
     is_completed: bool = False
+
+
+class UpdateTodo(SQLModel):
+    title: str | None
+    description: str | None
+    priority: int | None = None
+    is_completed: bool = False | None
